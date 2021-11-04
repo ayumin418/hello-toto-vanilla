c26e8a1239b834f70235a3c445395b3eeb985c86
Ayumi Nagata <89506244+ayumin418@users.noreply.github.com>
2021-11-05 07:12:35 +0900

    add complete-button task 完了ボタンの機能を追加

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  // alert(inputText);
 
   // liタグ生成
   const li = document.createElement("li");
@@ -13,19 +12,47 @@ const onClickAdd = () => {
 
   //button(完了)タグ生成
   const completeButtun = document.createElement("button");
-  completeButtun.innerHTML = "完了";
+  completeButtun.innerText = "完了";
+
+  // 完了ボタン機能
   completeButtun.addEventListener("click", () => {
-    alert("完了");
+    // 押された削除ボタンの親タグ（li）を未完了リストから削除
+    deletFromIncompleteList(completeButtun.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButtun.parentNode;
+
+    // TODO内容テキストを取得
+    const targetText = addTarget.firstElementChild.innerText;
+
+    // li以下を初期化
+    addTarget.textContent = null;
+
+    // pタグを生成
+    const p = document.createElement("p");
+    p.innerText = targetText;
+
+    // button（戻す）タグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // liタグの子要素に追加
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+    console.log(addTarget);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //button(削除)タグ生成
   const deleteButtun = document.createElement("button");
-  deleteButtun.innerHTML = "削除";
+  deleteButtun.innerText = "削除";
+
+  // 削除ボタン機能
   deleteButtun.addEventListener("click", () => {
     // 押された削除ボタンの親タグ（li）を未完了リストから削除
-    const deleteTarget = deleteButtun.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
-    // console.log(deleteTarget);
+    deletFromIncompleteList(deleteButtun.parentNode);
   });
 
   // pタグ生成
@@ -37,10 +64,14 @@ const onClickAdd = () => {
   li.appendChild(lead);
   li.appendChild(completeButtun);
   li.appendChild(deleteButtun);
-  console.log(li);
 
   //ulタグの子要素に各要素を設定
   document.getElementById("incomplete-list").appendChild(li);
+
+  // 未完了リストから指定の要素を削除
+  const deletFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 };
 
 document
