ea26f0d7ecc862063a6f26e9e453d4b58ed65ead
Ayumi Nagata <89506244+ayumin418@users.noreply.github.com>
2021-11-05 09:07:44 +0900

    add back-button task 戻るボタンの機能を追加

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import "./styles.css";
 
-// 追加ボタン
+// 追加ボタン機能
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  CreateIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deletFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const CreateIncompleteList = (text) => {
   // liタグ生成
   const li = document.createElement("li");
   li.className = "list-item-lead";
+
+  // pタグ生成
+  const lead = document.createElement("p");
+  lead.className = "list-lead";
+  lead.innerHTML = text;
 
   //button(完了)タグ生成
   const completeButtun = document.createElement("button");
@@ -35,11 +50,20 @@ const onClickAdd = () => {
     // button（戻す）タグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    // 戻すボタン機能
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ（li）を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // TODO内容テキストを取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      CreateIncompleteList(text);
+    });
 
     // liタグの子要素に追加
     addTarget.appendChild(p);
     addTarget.appendChild(backButton);
-    console.log(addTarget);
 
     // 完了リストに追加
     document.getElementById("complete-list").appendChild(addTarget);
@@ -48,30 +72,19 @@ const onClickAdd = () => {
   //button(削除)タグ生成
   const deleteButtun = document.createElement("button");
   deleteButtun.innerText = "削除";
-
   // 削除ボタン機能
   deleteButtun.addEventListener("click", () => {
     // 押された削除ボタンの親タグ（li）を未完了リストから削除
     deletFromIncompleteList(deleteButtun.parentNode);
   });
 
-  // pタグ生成
-  const lead = document.createElement("p");
-  lead.className = "list-lead";
-  lead.innerHTML = inputText;
-
   //liタグの子要素に各要素を設定
   li.appendChild(lead);
   li.appendChild(completeButtun);
   li.appendChild(deleteButtun);
 
-  //ulタグの子要素に各要素を設定
+  // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
-
-  // 未完了リストから指定の要素を削除
-  const deletFromIncompleteList = (target) => {
-    document.getElementById("incomplete-list").removeChild(target);
-  };
 };
 
 document
